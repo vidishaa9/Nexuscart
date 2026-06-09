3953129830b39d20cf65ed902ce57fa6a89371b0
vidishaa09 <vidishaarora14@gmail.com>
2026-06-09 11:43:25 +0530

    Add Product model class

---
+++ src/main/java/com/vidisha/ecommerceproject/Model/Product.java
@@ -0,0 +1,38 @@
+package com.vidisha.ecommerceproject.Model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String description;
+    private String brand;
+    private BigDecimal price;
+    private String category;
+    private Date releaseDate;
+    private boolean productAvailable;
+    private int stockQuantity;
+    private String imageName;
+    private String imageType;
+    @Lob //Large data
+    private byte[] imageData;
+
+    public Product(int id){
+        this.id=id;
+
+    }
+}
