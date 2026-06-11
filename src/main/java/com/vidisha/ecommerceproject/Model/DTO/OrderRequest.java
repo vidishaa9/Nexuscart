0da469da7025a8906800ee18edac7a526beb4236
vidishaa09 <vidishaarora14@gmail.com>
2026-06-11 23:36:11 +0530

    Add DTOs for order request and response

---
+++ src/main/java/com/vidisha/ecommerceproject/Model/DTO/OrderRequest.java
@@ -0,0 +1,9 @@
+//package com.vidisha.ecommerceproject.Model.DTO;
+//
+//import java.util.List;
+//
+//public record OrderRequest() {
+//    String customerName,
+//    String email,
+//    List<OrderItemRequest> items;
+//} {}
---
+++ src/main/java/com/vidisha/ecommerceproject/Model/DTO/OrderResponse.java
@@ -0,0 +1,13 @@
+//package com.vidisha.ecommerceproject.Model.DTO;
+//
+//import java.time.LocalDate;
+//import java.util.List;
+//
+//public class OrderResponse {
+//    String orderId,
+//    String customerName,
+//    String email,
+//    String status,
+//    LocalDate orderDate,
+//    List<OrderItemResponse> items
+//}
